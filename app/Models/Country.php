c2d026aa735f06f4e0a75d4f93306c43c25ef843
sunbtn <phpbtn2021@gmail.com>
2021-10-10 22:56:25 +0700

    CRUD Country

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Country extends Model
-{
-    use HasFactory;
+class Country extends Model {
+	use HasFactory;
+
+	protected $fillable = ['country_code', 'name'];
 }
